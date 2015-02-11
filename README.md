9ca01bd8689763fc699ffc22b428820ff31bc334
Jared Klingenberger <klinge2+git@g.clemson.edu>
2015-02-10 22:51:13 -0500

    Added test code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-functional-list
+functional-c-list
 ===============
 ### Author
 
@@ -9,10 +9,12 @@ twist, methods to interact with the list mimic functional programming style. For
 example, you can map a function over the entire list with a function passed as
 a parameter.
 
+It is important to note that it is impossible to ensure truly pure functions in
+C, so any operations will have absolutely no guarantee of purity.
+
 Common methods
 ==============
 * `void map(llist *l, nodefunc f)`
 * `void* llist_iter(llist *l, nodeiter f)`
 * `void* llist_test(llist *l, void *q, nodetest t)`
 * `void* llist_find(llist *l, void *data, nodetest equals)`
-
---
+++ llist_test.c
@@ -0,0 +1,79 @@
+/* Author: Jared Klingenberger <klinge2@clemson.edu> */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "llist.h"
+
+void iter_print(node *n) {
+  printf("value: %d\n", *(int*)n->data);
+}
+
+bool test_equals(void *a, void *b) {
+  return *(int*)a == *(int*)b;
+}
+
+void node_free(node *n) { free(n); }
+
+void llist_print(llist *l);
+bool find_and_remove(llist *l, int value);
+node *alloc_node(int *data);
+
+int main() {
+  llist *l = malloc(sizeof(llist));
+  llist_init(l);
+
+  int x = 4, y = 8;
+
+  printf("Adding some...\n");
+
+  llist_append(l, alloc_node(&x));
+  llist_append(l, alloc_node(&y));
+  llist_append(l, alloc_node(&x));
+  llist_append(l, alloc_node(&y));
+  llist_append(l, alloc_node(&x));
+  llist_append(l, alloc_node(&y));
+
+  llist_print(l);
+
+  printf("Removing some...\n");
+
+  int q = x, p = y+1;
+  if (!find_and_remove(l, q))
+    printf("Value %d not present in list\n", q);
+  if (!find_and_remove(l, p))
+    printf("Value %d not present in list\n", p);
+
+  void iter_remove(node *n) {
+    iter_print(n);
+    llist_remove(l, n);
+    node_free(n);
+  }
+  map(l, iter_remove);
+
+  llist_print(l);
+
+  llist_destroy(l, node_free);
+  free(l);
+
+  return 0;
+}
+
+void llist_print(llist *l) {
+  map(l, iter_print);
+}
+
+bool find_and_remove(llist *l, int value) {
+  node *n = llist_find(l, &value, test_equals);
+  if (n) {
+    llist_remove(l, n);
+    node_free(n);
+  }
+  return (n != NULL);
+}
+
+node *alloc_node(int *data) {
+  node *n = malloc(sizeof(node));
+  llist_node_init(n);
+  n->data = data;
+  return n;
+}
